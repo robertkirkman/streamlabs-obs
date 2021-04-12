554f12921bdcebe3ad3a5a724de2b25ffda87806
Robert Kirkman <robert.kirkman@ttu.edu>
2021-04-11 19:16:49 -0700

    Greatly enlarged size of Recording button, changed colors of Recording button, and changed text on both Recording and Streaming buttons

--- app/components/StudioFooter.vue
+++ app/components/StudioFooter.vue
@@ -34,7 +34,7 @@
           @click="toggleRecording"
           :class="{ active: streamingService.isRecording }"
         >
-          <span>REC</span>
+          <span>RECORD</span>
         </button>
       </div>
       <div class="nav-item" v-if="replayBufferEnabled && replayBufferOffline">
@@ -163,27 +163,31 @@
   .weight(@bold);
 
   position: relative;
-  width: 30px;
-  height: 30px;
-  background-color: var(--button);
+  width: 400px;
+  height: 200px;
+  background-color: var(--warning);
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 100%;
+  border-radius: 20% 20% 20% 20%;
   box-sizing: content-box;
-  letter-spacing: 0.2px;
+  letter-spacing: 2px;
 
   span {
-    font-size: 10px;
-    color: var(--warning);
+    font-size: 100px;
+    color: var(--white);
   }
 
   &:hover {
-    background-color: var(--button-hover);
+    background-color: var(--warning);
+
+    span {
+      color: var(--white);
+    }
   }
 
   &.active {
-    animation: pulse 2.5s linear infinite;
+    animation: pulse 0.5s linear infinite;
     background-color: var(--warning);
 
     span {
@@ -193,6 +197,7 @@
 
   &:focus {
     outline: none;
+    background-color: var(--warning);
   }
 }
 
